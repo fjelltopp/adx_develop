5312718b9b826751648cc76481d41fa1daed28fb
gulfa <gunnarroe@gmail.com>
2019-02-20 11:51:17 +0200

    Initial DB setup

--- ckan/ckan-entrypoint-adx.sh
+++ ckan/ckan-entrypoint-adx.sh
@@ -65,4 +65,8 @@ fi
 ckan-pip install -e /usr/lib/adx/ckanext-unaids
 ckan-pip install -e /usr/lib/adx/ckanext-cloudstorage
 
+set_environment
+ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
+
+
 exec "$@"
