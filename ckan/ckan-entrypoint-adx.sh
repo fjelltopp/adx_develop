cb310d596e9a1c638d74249749504db1d11b88ab
Tomasz Sabała <sabala.tomek@gmail.com>
2020-10-13 12:13:28 +0200

    ADX-284 tmp removing googleanalytics extenstion.

--- ckan/ckan-entrypoint-adx.sh
+++ ckan/ckan-entrypoint-adx.sh
@@ -77,7 +77,6 @@ ckan-pip install -e /usr/lib/adx/ckanext-geoview
 ckan-pip install -e /usr/lib/adx/ckanext-issues
 ckan-pip install --no-deps -e /usr/lib/adx/ckanext-file_uploader_ui
 ckan-pip install --no-deps -e /usr/lib/adx/ckanext-emailasusername
-ckan-pip install -e /usr/lib/adx/ckanext-googleanalytics
 
 set_environment
 ckan  --config "${CKAN_CONFIG}/production.ini" db init
