0538141adba36344cffdc940cd520f4efa7b64e0
Tomasz Sabała <sabala.tomek@gmail.com>
2020-10-20 15:38:16 +0200

    Calling legacy paster commands for extensions.

--- util/__init__.py
+++ util/__init__.py
@@ -142,14 +142,16 @@ def setup(args, extra):
 
 
 def init_ckan_db(args, extra):
+    call_command(['docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini db init'])
+    call_command(['docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckanext-ytp-request initdb -c /etc/ckan/production.ini'])
+    call_command(['docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckanext-harvest harvester initdb -c /etc/ckan/production.ini'])
+    call_command(['docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckanext-validation validation init-db -c /etc/ckan/production.ini'])
+    call_command(['docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckanext-issues issues init_db -c /etc/ckan/production.ini '])
+
     call_command(['docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini datastore set-permissions | docker exec -i db psql -U ckan'])
-    call_command(['docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini ytp-request initdb'])
-    call_command(['docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini harvester initdb'])
-    call_command(['docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini validation init-db'])
     call_command(['docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini user add admin email=admin@localhost name=admin password=fjelltopp apikey=a4bf5640-e1b2-4141-8c22-f2b96b6df2c3'])
     call_command([f'docker exec db psql -U {PG_USER} -c "UPDATE public.user SET apikey = \'{ADMIN_APIKEY}\' WHERE name = \'admin\';"'])
     call_command(['docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini sysadmin add admin'])
-    call_command(['docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini issues init_db'])
 
 
 def load_demo_data(args, extra):
