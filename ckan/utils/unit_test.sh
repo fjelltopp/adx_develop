58968bf6066f8fde2130096967bf9124153e7088
Tomasz Sabała <sabala.tomek@gmail.com>
2020-12-10 10:02:29 +0100

    ADX-551 Remove unused helper. Fixed reset solr helper.

--- ckan/utils/unit_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-module=$1
-
-/usr/local/bin/ckan-nosetests --ckan --with-pylons=/usr/lib/adx/ckanext-${module}/test.ini /usr/lib/adx/ckanext-${module}/ckanext/${module}/tests --logging-level=WARNING
