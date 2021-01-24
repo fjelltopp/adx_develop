6b56d2cab2e8ea32d7374a37ebe3790758905cb1
Michal Ful <mful@it-studio.com.pl>
2021-01-24 15:33:00 +0100

    CKAN core tests, cleanup Workspace even on fail, better docker cleanup

--- ci.sh
+++ ci.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Docker cleanup"
+docker-compose down --rmi all -v --remove-orphans
 
 echo "Preparing environment"
-
-yes | ./adx setup
+cd ../
+export PATH=$WORKSPACE/adx_develop/:$PATH
+yes | adx setup
 #if [ ! -e "$(dirname "${PWD}")"/datapusher ]; then
 #  ln -s "${PWD}"/datapusher "$(dirname "${PWD}")"/datapusher
 #fi
@@ -15,20 +18,22 @@ yes | ./adx setup
 #fi
 
 echo "running ./adx build"
-./adx build
+adx build
 echo "running ./adx up"
-./adx up
+adx up
 echo "tunning ./adx/dbsetup"
-./adx dbsetup
+adx dbsetup
 echo "running ./adx restart ckan"
-./adx restart ckan
+adx restart ckan
 
 echo "Show docker-compose containers"
 docker-compose ps
 echo "Running ./adx testsetup"
-./adx testsetup
+adx testsetup
+echo "Run ckan core tests"
+docker exec -it ckan ckan-nosetests --ckan --with-pylons=/usr/lib/ckan/venv/src/ckan/test-core.ini ckan ckanext
 echo "Running ./adx test restricted"
-./adx test restricted
+adx test restricted
 
 #echo "Cleanup"
 #rm -f "$(dirname "${PWD}")"/datapusher
