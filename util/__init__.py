7da9957e9c03a7736163eecaddcfe7c160592376
Tomasz Sabała <tomek@fjelltopp.org>
2021-03-22 08:35:55 +0100

    Remove unused variable.

--- util/__init__.py
+++ util/__init__.py
@@ -26,10 +26,7 @@ ADX_PATH = os.path.expanduser(os.environ.get(
 PG_USER = os.environ.get('PG_USER', 'ckan')
 CKAN_TEST_SQLALCHEMY_URL = os.environ.get("CKAN_TEST_SQLALCHEMY_URL", "postgresql://ckan_default:pass@db/ckan_test")
 ADMIN_APIKEY = os.environ.get("ADMIN_APIKEY", "6011357f-a7f8-4367-a47d-8c2ab8059520")
-CKAN_SITE_URL = os.environ.get("CKAN_SITE_URL", "http://dev-adr:5000")
-CKAN_HOST_PORT = CKAN_SITE_URL.lstrip('http://')
-if len(CKAN_HOST_PORT.split(':')) == 1:
-    CKAN_HOST_PORT = f"{CKAN_HOST_PORT}:80"
+CKAN_SITE_URL = os.environ.get("CKAN_SITE_URL", "http://dev-adr")
 
 def call_command(args):
     """
