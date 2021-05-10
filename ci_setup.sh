0815c658c7d85ce41e2c8990d50a54d0ce82b679
Michal Ful <michal.ful@gmail.com>
2021-05-10 15:14:13 +0200

    Added slack notifications and fixes related to pull request based builds

--- ci_setup.sh
+++ ci_setup.sh
@@ -4,13 +4,24 @@
 echo "Docker cleanup"
 docker-compose down --rmi all -v --remove-orphans
 
+if [ -v "$CHANGE_ID" ]
+then
+  BRANCH="$GIT_BRANCH"
+else 
+  BRANCH="origin/pr/$CHANGE_ID"
+fi
+
 echo "Preparing environment"
 cd ../
 # add adx script to PATH
 export PATH=$WORKSPACE/adx_develop/:$PATH
+# prepare environment
+cp "$WORKSPACE"/adx_develop/dev.env "$WORKSPACE"/adx_develop/.env
 # Setup environment
 yes | adx setup
-git checkout "${GIT_BRANCH}"
+git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/* && git checkout "${BRANCH}"
+cd "$WORKSPACE"/adx_develop/ && git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/* && git checkout "${BRANCH}" 
+cd "$WORKSPACE" || exit
 echo "running ./adx build"
 adx build
 echo "running ./adx up"
