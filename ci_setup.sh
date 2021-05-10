0efb95d3884e76acdfa9eaa9310e09c3f542827c
Michal Ful <michal.ful@gmail.com>
2021-05-10 12:27:06 +0200

    Pull request build support

--- ci_setup.sh
+++ ci_setup.sh
@@ -4,6 +4,13 @@
 echo "Docker cleanup"
 docker-compose down --rmi all -v --remove-orphans
 
+if [ -v "$CHANGE_ID" ]
+then
+  BRANCH="origin/pr/$CHANGE_ID"
+else 
+  BRANCH="$GIT_BRANCH"
+fi
+
 echo "Preparing environment"
 cd ../
 # add adx script to PATH
@@ -12,8 +19,8 @@ export PATH=$WORKSPACE/adx_develop/:$PATH
 cp "$WORKSPACE"/adx_develop/dev.env "$WORKSPACE"/adx_develop/.env
 # Setup environment
 yes | adx setup
-git checkout "${GIT_BRANCH}"
-cd "$WORKSPACE"/adx_develop/ && git checkout "${GIT_BRANCH}" 
+git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/* && git checkout "${BRANCH}"
+cd "$WORKSPACE"/adx_develop/ && git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/* && git checkout "${BRANCH}" 
 cd "$WORKSPACE" || exit
 echo "running ./adx build"
 adx build
