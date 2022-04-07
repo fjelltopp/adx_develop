2092afad9884c31ac681c00ad3e371a2b5012d4b
Michal Ful <michal@fjelltopp.org>
2022-04-07 15:03:27 +0200

    Working pipenv with external CKAN base image

--- ckan/bootstrap.sh
+++ ckan/bootstrap.sh
@@ -8,4 +8,7 @@ pipenv sync --dev
 ls -la
 cd "$HOME"
 rm -rf venv;
-ln -s .adxvenv/adx_develop-YYZTJvBg venv;
+ln -s .adxvenv/adx_develop-YYZTJvBg venv
+ln -s "$HOME"/venv/bin/ckan /usr/local/bin/ckan
+ln -s "$HOME"/venv/bin/paster /usr/local/bin/ckan-paster
+# chown -R ckan:ckan ./
